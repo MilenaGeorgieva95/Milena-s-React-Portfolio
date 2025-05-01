3df3c52e0d948cac8b2cebc79b2541b687dcd4de
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-01 23:20:21 +0300

    add favicon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,13 +11,7 @@ export default function Header() {
           <strong>I am Milena Georgieva, </strong>
           <br />
           Software Developer with solid grounding in React.js, JavaScript and
-          SPA applications,
-          <br />
-          currently at the end of the <br />
-          <a href="https://softuni.bg/">
-            SoftUni Software Engineering Program
-          </a>{" "}
-          .
+          SPA applications.
         </h1>
         <ContactLinks />
       </div>
