8f3d76b4fd4d5b62e9625350f9b2408f15f56a48
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-08 14:55:34 +0300

    add links to project and github repo

--- src/projectsData/projectsData.js
+++ src/projectsData/projectsData.js
@@ -2,7 +2,7 @@ const projects = [
   {
     id: 1,
     name: "Sprout Hub",
-    href: "https://sprout-hub-458309.web.app/posts",
+    deployedLink: "https://sprout-hub-458309.web.app/posts",
     codeRepo: "https://github.com/MilenaGeorgieva95/Sprout-Hub",
     imageSrc:
       "https://media.licdn.com/dms/image/v2/D4D2DAQFmimya8kI03w/profile-treasury-image-shrink_800_800/B4DZaMvqOAGsAc-/0/1746118027180?e=1746738000&v=beta&t=69B8ab-A6yy9nPRjbrgGYPHMFkXMCjL_HcFV42YwL58",
@@ -16,22 +16,23 @@ const projects = [
     ],
     highlights: [1, 2],
     techUsed: "React.js, JavaScript ",
-    repo: "abc",
-    link: "cba",
   },
   {
     id: 2,
-    name: "Sprout Hub",
-    href: "https://sprout-hub-458309.web.app/posts",
-    codeRepo: "https://github.com/MilenaGeorgieva95/Sprout-Hub",
-    imageSrc:
-      "https://media.licdn.com/dms/image/v2/D4D2DAQFmimya8kI03w/profile-treasury-image-shrink_800_800/B4DZaMvqOAGsAc-/0/1746118027180?e=1746738000&v=beta&t=69B8ab-A6yy9nPRjbrgGYPHMFkXMCjL_HcFV42YwL58",
+    name: "Sole Mates",
+    deployedLink: "https://sole-mates.github.io/",
+    codeRepo: "https://github.com/sole-mates/sole-mates.github.io",
+    imageSrc: "/images/SM-home.png",
     description:
-      "Sprout Hub is a modern Single Page Application built with React.js, providing an interactive platform for users to create, manage, and engage with content. The project follows best practices in front-end development.",
-    imageAlt: "Sprout Hub App overview image.",
-    imagesDetails: ["/images/SH-browse.png"],
+      "Sole Mates SPA project with REST, CRUD, Authentication and Search functionality. Implemented page.js and lit-html libraries. SPA application task from the JS Applications course @SoftUni.",
+    imageAlt: "Sole Mates App overview image.",
+    imagesDetails: [
+      "/images/SM-register.png",
+      "/images/SM-search.png",
+      "/images/SM-add-pair.png",
+    ],
     highlights: [1, 2],
-    techUsed: "fasda",
+    techUsed: "Page.js, lit-html, Mocha, Chai",
   },
 ];
 
