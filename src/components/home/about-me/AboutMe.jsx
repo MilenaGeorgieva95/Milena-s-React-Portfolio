197678edc62cf06275e42d79137f2a0ebf8ba1b3
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-08 14:55:16 +0300

    add about me paragraph

--- src/components/home/about-me/AboutMe.jsx
+++ src/components/home/about-me/AboutMe.jsx
@@ -1,24 +1,30 @@
+import { Link } from "react-router";
+
 export default function AboutMe() {
   return (
     <section id="one">
       <header className="major">
         <h2>
-          Ipsum lorem dolor aliquam ante commodo
+          Junior Software Developer
           <br />
-          magna sed accumsan arcu neque.
         </h2>
       </header>
       <p>
-        Software Developer currently at the end of the @SoftUni Software
-        Engineering Program. Completed Web Developer Bootcamp 2023 with
-        JavaScript @Udemy. I have strong base in React.js, JavaScript, NodeJS,
-        MongoDB.
+        qualified in ReactJS, Node.js and JavaScript with over 2 years of
+        academic experience creating projects and solving tasks using HTML, CSS,
+        ReactJS, JavaScript, Node.js and Express. Currently at the end of a
+        Full-Stack JavaScript Web Developer program at SoftUni.Eager to
+        contribute to collective success, through innovation, hard work,
+        attention to detail and excellent organizational skills. Experience
+        using MongoDB, MySQL, GitHub, Tailwind and Bootstrap. Professional
+        English speaker with a positive approach to tasks, working well under
+        pressure, deadline orientated, ambitious with a hands on approach.
       </p>
       <ul className="actions">
         <li>
-          <a href="#" className="button">
+          <Link to="/cv" className="button">
             Learn More
-          </a>
+          </Link>
         </li>
       </ul>
     </section>
