55349d0ee4298489a7ba29e6e49752fb83c2d1bc
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-09-03 14:53:01 +0300

    style projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,38 +1,66 @@
 import { Link } from "react-router";
 import projects from "../../projectsData/projectsData.js";
+import styles from "./Projects.module.css";
 
 export default function Projects() {
   return (
-    <>
-      <h2>My Projects</h2>
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">My Projects</h2>
-
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-8">
-            {projects.map((project) => (
+    <div className={styles.projectsContainer}>
+      <h2>MY PROJECTS</h2>
+      <div className={styles.projectsWrapper}>
+        {projects.map((project) => (
+          <div className={styles.projectCard}>
+            <section>
               <Link
                 key={project.id}
                 to={`/projects/${project.id}/details`}
                 className="group"
               >
-                <img
-                  alt={project.imageAlt}
-                  src={project.imageSrc}
-                  className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
-                />
-                <h3 className="mt-4 text-sm text-gray-700">{project.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  Details
-                  {/* <Link to={project.codeRepo}>
-                    <i className="fa-solid fa-code"></i>
-                  </Link> */}
-                </p>
+                <h3>{project.name}</h3>
+                <img alt={project.imageAlt} src={project.imageSrc} />
               </Link>
-            ))}
+            </section>
+
+            <section class={styles.detailsSection}>
+              <div class={styles.projectLinks}>
+                <ul>
+                  <li>
+                    <Link to={project.codeRepo}>
+                      GitHub Repository: <span>{project.codeRepo}</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={project.deployedLink}>
+                      Live Link: <span>{project.deployedLink}</span> (deployed
+                      on Firebase)
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <p>{project.description}</p>
+              </div>
+              <div>
+                <p>{project.techUsed}</p>
+              </div>
+              <div class={styles.buttonsContainer}>
+                <Link
+                  to={project.codeRepo}
+                  class={styles.githubBtn + " " + styles.detailsBtn}
+                >
+                  GitHub Repository
+                </Link>
+                <Link
+                  to={`/projects/${project.id}/details`}
+                  class={styles.detailsBtn}
+                >
+                  Details
+                </Link>
+              </div>
+            </section>
           </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
