406c6bcc135da029c197bacddd3bbf0d8ead2edf
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-12-11 14:12:47 +0200

    style My Latest Projects section

--- src/components/home/recent-work/RecentWork.jsx
+++ src/components/home/recent-work/RecentWork.jsx
@@ -1,32 +1,38 @@
 import { Link } from "react-router";
-// https://sprout-hub-458309.web.app/
 import projects from "../../../projectsData/projectsData.js";
+import styles from "./RecentWork.module.css";
 
 export default function RecentWork() {
   return (
-    <section id="two">
-      <h2>Recent Work</h2>
-      <div className="lg:grid lg:grid-cols-1 lg:gap-y-8">
+    <section class={styles.wrapper}>
+      <h2 class="section-title">My Latest Projects</h2>
+      <div>
         {projects.map((project) => {
           return (
-            <article
-              key={project.id}
-              className="col-6 col-12-xsmall work-item hover:opacity-55"
-            >
-              <Link to={`projects/${project.id}/details`}>
-                <img
-                  className="aspect-3/2 w-full rounded-lg object-cover"
-                  src={project.imageSrc}
-                  alt="project overview image"
-                />
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
+            <article key={project.id}>
+              <Link
+                to={`projects/${project.id}/details`}
+                class={styles.projectCard}
+              >
+                
+
+                <div  class={styles.projectCardInfo}>
+                  <h3>{project.name}</h3>
+                  <div class={styles.description}>
+                  <p >{project.description}</p>
+                  <div>Details</div>
+                  </div>
+
+                </div>
+                <div  class={styles.projectCardImg}>
+                  <img src={project.imageSrc} alt="project overview image" />
+                </div>
               </Link>
             </article>
           );
         })}
       </div>
-      <ul className="actions">
+      <ul>
         <li>
           <Link to="/projects" className="button">
             Full Portfolio
